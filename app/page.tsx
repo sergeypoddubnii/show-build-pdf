4f9d6bddbedf734f3378c5bd5cb4b91c3b11fa62
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-10-23 22:16:21 +0300

    add packages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from 'next/image'
+'use client';
+import html2canvas from 'html2canvas';
+import {useEffect, useRef} from "react";
+
 
 export default function Home() {
-  return (
-    <main>
-      SHOW-BUILD-PDF
-    </main>
-  )
+    const canvasRef = useRef();
+
+    const handleGetPDF = () => {
+
+        const ctx = canvasRef.current.getContext('2d');
+    }
+
+    return (
+        <main>
+            <div>
+                <canvas ref={canvasRef}/>
+                <button onClick={handleGetPDF}>
+                    handleGetPDF
+                </button>
+            </div>
+        </main>
+    )
 }
