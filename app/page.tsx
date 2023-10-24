02c05185b4952253084593ecd0fdf5584e2883c0
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-10-24 20:38:47 +0300

    remove canvas

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,10 @@ import {useEffect, useRef} from "react";
 
 
 export default function Home() {
-    const canvasRef = useRef();
-
-    const handleGetPDF = () => {
-
-        const ctx = canvasRef.current.getContext('2d');
-    }
 
     return (
         <main>
-            <div>
-                <canvas ref={canvasRef}/>
-                <button onClick={handleGetPDF}>
-                    handleGetPDF
-                </button>
-            </div>
+            canvas
         </main>
     )
 }
