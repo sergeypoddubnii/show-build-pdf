b4a6c2d74352947863144ecba29ba4aa5bd63749
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-11-20 22:04:37 +0200

    add odf

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <main>
             canvas
-            <h1>Generate PDF with React-pdf</h1>
+            <h1>Generate PDF with pdf</h1>
         </main>
     )
 }
