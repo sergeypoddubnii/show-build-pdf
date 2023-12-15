962636c8e8625f3b42fbed5f1630a920d96cd9cf
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-12-15 21:11:33 +0200

    remove boilerplate

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
             >
                 {({ blob, url, loading, error }) => {
                     if(!url) return;
+
                     setUrlFile(url);
                     return (loading ? 'Loading document...' : 'Download now!')
                 }}
