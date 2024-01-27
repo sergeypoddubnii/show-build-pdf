275b0ae7f3fe5b9d9a863d5ec6f281d97e104219
sergey poddubnii <poddubnii.sergey@gmail.com>
2024-01-27 21:49:22 +0200

    fix title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'show-build-pd',
+  title: 'show-build-pdf',
   description: 'Pdf builder',
 }
 
