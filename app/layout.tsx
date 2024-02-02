6eb4a2f6e959e38d797a64b7db8acabd77118445
sergey poddubnii <poddubnii.sergey@gmail.com>
2024-02-02 20:52:45 +0200

    delete description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'build-cv',
-  description: 'some description',
+  description: '',
 }
 
 export default function RootLayout({
