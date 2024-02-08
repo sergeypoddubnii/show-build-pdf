fe4e6b8d04232f0fecf5c83b9ca91e7d84881428
sergey poddubnii <poddubnii.sergey@gmail.com>
2024-02-08 20:05:54 +0200

    add new description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'build-cv',
-  description: '',
+  description: 'jhggjgj',
 }
 
 export default function RootLayout({
