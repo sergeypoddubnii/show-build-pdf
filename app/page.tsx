92052300c27e7272504db84de4874dd6acbc1a80
sergey poddubnii <poddubnii.sergey@gmail.com>
2024-03-16 20:26:17 +0200

    check download file

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
                     if(!url) return;
 
                     setUrlFile(url);
-                    return (loading ? 'Loading' : 'Download now!!!')
+                    return (loading ? 'Loading' : 'Download now')
                 }}
             </PDFDownloadLink>
             <TextEditor
