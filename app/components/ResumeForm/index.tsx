49067f6891ffd64b3bd62a9bce7c8cb3fcbff6a8
serhiipiddubnii <poddubnii.sergey@gmail.com>
2025-02-13 20:32:52 +0200

    small refactor

---
+++ app/components/ResumeForm/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ResumeForm = () => {
+    return (
+        <div className="w-1">
+            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs"/>
+            <button className="btn btn-primary">Button</button>
+        </div>
+    );
+};
+
+export default ResumeForm;
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -3,33 +3,25 @@ import {PDFDownloadLink} from '@react-pdf/renderer';
 import {useState} from "react";
 import PdfDocument from "@/app/components/PdfDocument/index";
 import PdfViewer from "@/app/components/PdfViewer/index";
-import TextEditor from "@/app/components/TextEditor/index";
+import ResumeForm from "@/app/components/ResumeForm";
 
 export default function ResumePage() {
     const [urlFile, setUrlFile] = useState('');
-    const [valueEditor, setValueEditor] = useState('');
-
-    const handleChange = (val:string) => {
-        setValueEditor(val)
-    };
 
     return (
         <main>
+            <ResumeForm/>
             <PDFDownloadLink
-                document={<PdfDocument  valueEditor={valueEditor}/>}
+                document={<PdfDocument valueEditor={''}/>}
                 fileName="hashfilename.pdf"
             >
-                {({ url, loading, error }) => {
+                {({ url, loading }) => {
                     if(!url) return;
 
                     setUrlFile(url);
                     return (loading ? 'Loading' : 'Download now')
                 }}
             </PDFDownloadLink>
-            <TextEditor
-                value={valueEditor}
-                handleChange={handleChange}
-            />
             {urlFile ? <PdfViewer url={urlFile} /> : null}
         </main>
     )
