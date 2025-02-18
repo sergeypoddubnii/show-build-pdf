c64d0208f7468a9324a35466a1f16737281e288d
serhiipiddubnii <poddubnii.sergey@gmail.com>
2025-02-18 21:36:53 +0200

    Revert "small refactor"
    This reverts commit 49067f6891ffd64b3bd62a9bce7c8cb3fcbff6a8.

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -3,25 +3,33 @@ import {PDFDownloadLink} from '@react-pdf/renderer';
 import {useState} from "react";
 import PdfDocument from "@/app/components/PdfDocument/index";
 import PdfViewer from "@/app/components/PdfViewer/index";
-import ResumeForm from "@/app/components/ResumeForm";
+import TextEditor from "@/app/components/TextEditor/index";
 
 export default function ResumePage() {
     const [urlFile, setUrlFile] = useState('');
+    const [valueEditor, setValueEditor] = useState('');
+
+    const handleChange = (val:string) => {
+        setValueEditor(val)
+    };
 
     return (
         <main>
-            <ResumeForm/>
             <PDFDownloadLink
-                document={<PdfDocument valueEditor={''}/>}
+                document={<PdfDocument  valueEditor={valueEditor}/>}
                 fileName="hashfilename.pdf"
             >
-                {({ url, loading }) => {
+                {({ url, loading, error }) => {
                     if(!url) return;
 
                     setUrlFile(url);
                     return (loading ? 'Loading' : 'Download now')
                 }}
             </PDFDownloadLink>
+            <TextEditor
+                value={valueEditor}
+                handleChange={handleChange}
+            />
             {urlFile ? <PdfViewer url={urlFile} /> : null}
         </main>
     )
