6bee1063a7cdd902487735c7da6bcb4ff1076442
serhiipiddubnii <poddubnii.sergey@gmail.com>
2025-02-28 21:52:21 +0200

    refactor pdf viewer

--- app/components/PdfViewer/index.tsx
+++ app/components/PdfViewer/index.tsx
@@ -4,38 +4,37 @@ import React, { useState, useRef } from 'react';
 import { usePdf } from '@mikecousins/react-pdf';
 
 interface IProps {
-    url:string;
+    url: string;
 }
 
 const PdfViewer = ({url}:IProps) => {
     const [page, setPage] = useState(1);
     const canvasRef = useRef(null);
 
-    const { pdfDocument } = usePdf({
-        file: url,
-        page,
-        canvasRef,
-    });
+    const { pdfDocument } = usePdf({file: url, page, canvasRef});
+
+    const handleNextPage = () => setPage((prev) => prev + 1);
+    const handlePrevPage = () => setPage((prev) => prev - 1);
 
     return (
         <div>
             {!pdfDocument && <span>Loading...</span>}
             <canvas
                 ref={canvasRef}
-                style={{transform: 'scale(0.7)'}}
+                style={{transform: 'scale(0.8)'}}
             />
             {Boolean(pdfDocument && pdfDocument.numPages) && (
                 <nav>
                     <ul className="pager">
                         <li className="previous">
-                            <button disabled={page === 1} onClick={() => setPage(page - 1)}>
+                            <button disabled={page === 1} onClick={handlePrevPage}>
                                 Previous
                             </button>
                         </li>
                         <li className="next">
                             <button
                                 disabled={page === pdfDocument.numPages}
-                                onClick={() => setPage(page + 1)}
+                                onClick={handleNextPage}
                             >
                                 Next
                             </button>
