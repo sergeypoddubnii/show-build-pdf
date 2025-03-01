dada47ee550a43b4b7635cac3cd4226960abe0f6
serhiipiddubnii <poddubnii.sergey@gmail.com>
2025-03-01 23:08:25 +0200

    add daisyui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-'use client';
-
 export default function Home() {
     return (
         <main>
-            just home page
+            <button className="btn btn-primary">One</button>
+            <button className="btn btn-secondary">Two</button>
+            <button className="btn btn-accent btn-outline">Three</button>
         </main>
     )
 }
